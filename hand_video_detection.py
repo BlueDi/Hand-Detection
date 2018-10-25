6c32f42fd69433441eadc5641d3e4a853052691e
Diogo Cruz <diogohmcruz@hotmail.com>
2018-10-25 14:25:37 +0100

    Improve formating

--- hand_video_detection.py
+++ hand_video_detection.py
@@ -18,7 +18,7 @@ while (True):
         kernel = np.ones((3, 3), np.uint8)
 
         # define region of interest
-		#roi = frame[100:300, 100:300]
+        #roi = frame[100:300, 100:300]
         roi = frame[50:300, 300:550]
 
         #cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
@@ -41,7 +41,8 @@ while (True):
         erosion = cv2.GaussianBlur(erosion, (5, 5), 100)
 
         # find contours
-        _, contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        _, contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE,
+                                                  cv2.CHAIN_APPROX_SIMPLE)
 
         # find contour of max area(hand)
         cnt = max(contours, key=lambda x: cv2.contourArea(x))
@@ -75,9 +76,9 @@ while (True):
             pt = (100, 180)
 
             # find length of all sides of triangle
-            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
             s = (a + b + c) / 2
             ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
@@ -85,7 +86,7 @@ while (True):
             d = (2 * ar) / a
 
             # apply cosine rule here
-            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
+            angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
 
             # ignore angles > 90 and ignore points very close to convex hull
             # they generally come due to noise)
@@ -101,29 +102,40 @@ while (True):
         font = cv2.FONT_HERSHEY_SIMPLEX
         if l == 1:
             if areacnt < 2000:
-                cv2.putText(frame, 'Put hand in the box', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                cv2.putText(frame, 'Put hand in the box', (0, 50), font, 2,
+                            (0, 0, 255), 3, cv2.LINE_AA)
             else:
                 if arearatio < 12:
-                    cv2.putText(frame, '0', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                    cv2.putText(frame, '0', (0, 50), font, 2, (0, 0, 255), 3,
+                                cv2.LINE_AA)
                 elif arearatio < 17.5:
-                    cv2.putText(frame, 'Fixe', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                    cv2.putText(frame, 'Fixe', (0, 50), font, 2, (0, 0, 255),
+                                3, cv2.LINE_AA)
                 else:
-                    cv2.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                    cv2.putText(frame, '1', (0, 50), font, 2, (0, 0, 255), 3,
+                                cv2.LINE_AA)
         elif l == 2:
-            cv2.putText(frame, '2', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.putText(frame, '2', (0, 50), font, 2, (0, 0, 255), 3,
+                        cv2.LINE_AA)
         elif l == 3:
             if arearatio < 27:
-                cv2.putText(frame, '3', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                cv2.putText(frame, '3', (0, 50), font, 2, (0, 0, 255), 3,
+                            cv2.LINE_AA)
             else:
-                cv2.putText(frame, 'ok', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+                cv2.putText(frame, 'ok', (0, 50), font, 2, (0, 0, 255), 3,
+                            cv2.LINE_AA)
         elif l == 4:
-            cv2.putText(frame, '4', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.putText(frame, '4', (0, 50), font, 2, (0, 0, 255), 3,
+                        cv2.LINE_AA)
         elif l == 5:
-            cv2.putText(frame, '5', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.putText(frame, '5', (0, 50), font, 2, (0, 0, 255), 3,
+                        cv2.LINE_AA)
         elif l == 6:
-            cv2.putText(frame, 'reposition', (0, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.putText(frame, 'reposition', (0, 50), font, 2, (0, 0, 255), 3,
+                        cv2.LINE_AA)
         else:
-            cv2.putText(frame, 'reposition', (10, 50), font, 2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.putText(frame, 'reposition', (10, 50), font, 2, (0, 0, 255), 3,
+                        cv2.LINE_AA)
 
         # show the windows
         cv2.imshow('before erosion', mask)
@@ -140,5 +152,3 @@ while (True):
 
 video_capture.release()
 cv2.destroyAllWindows()
-
-
