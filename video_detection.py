8a9b68a97bcd9187cdc8aec56994922c7df63d66
Diogo Cruz <diogohmcruz@hotmail.com>
2018-10-29 15:33:16 +0000

    analyse_defect as a function

--- video_detection.py
+++ video_detection.py
@@ -34,35 +34,7 @@ def hand_detection(lower_bound_color, upper_bound_color, left=False):
 
             cnt = max(contours, key=lambda x: cv2.contourArea(x))
 
-            epsilon = 0.0005 * cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, epsilon, True)
-
-            hull = cv2.convexHull(approx, returnPoints=False)
-            defects = cv2.convexityDefects(approx, hull)
-
-            l = 0
-            for i in range(defects.shape[0]):
-                s, e, f, d = defects[i, 0]
-                start = tuple(approx[s][0])
-                end = tuple(approx[e][0])
-                far = tuple(approx[f][0])
-                pt = (100, 180)
-
-                a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-                b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-                c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-                s = (a + b + c) / 2
-                ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
-
-                d = (2 * ar) / a
-
-                angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
-
-                if angle <= 90 and d > 30:
-                    l += 1
-                    cv2.circle(roi, far, 3, [255, 0, 0], -1)
-                cv2.line(roi, start, end, [0, 255, 0], 2)
-
+            l = analyse_defects(cnt)
             analyse_contours(frame, cnt, l + 1)
 
             show_results(mask, erosion, frame)
@@ -76,6 +48,38 @@ def hand_detection(lower_bound_color, upper_bound_color, left=False):
             video_capture.release()
             cv2.destroyAllWindows()
             break
+
+
+def analyse_defects(cnt):
+    epsilon = 0.0005 * cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, epsilon, True)
+
+    hull = cv2.convexHull(approx, returnPoints=False)
+    defects = cv2.convexityDefects(approx, hull)
+
+    l = 0
+    for i in range(defects.shape[0]):
+        s, e, f, d = defects[i, 0]
+        start = tuple(approx[s][0])
+        end = tuple(approx[e][0])
+        far = tuple(approx[f][0])
+        pt = (100, 180)
+
+        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+        s = (a + b + c) / 2
+        ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+        d = (2 * ar) / a
+
+        angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
+
+        if angle <= 90 and d > 30:
+            l += 1
+            cv2.circle(roi, far, 3, [255, 0, 0], -1)
+        cv2.line(roi, start, end, [0, 255, 0], 2)
+    return l
 
 
 def analyse_contours(frame, cnt, l):
