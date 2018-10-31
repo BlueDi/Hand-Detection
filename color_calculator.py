b8f84aca2bb7f1b7f629a73c454d93531a9d45e8
Miguel Lucas <cmiguel.lucas@gmail.com>
2018-10-31 17:17:59 +0000

    Removed print

--- color_calculator.py
+++ color_calculator.py
@@ -51,11 +51,6 @@ def captureCamera(left=False):
 
     cap.release()
     hsvRoi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    print(
-        'min H = {}, min S = {}, min V = {}; max H = {}, max S = {}, max V = {}'
-        .format(hsvRoi[:, :, 0].min(), hsvRoi[:, :, 1].min(),
-                hsvRoi[:, :, 2].min(), hsvRoi[:, :, 0].max(),
-                hsvRoi[:, :, 1].max(), hsvRoi[:, :, 2].max()))
 
     lower = np.array(
         [hsvRoi[:, :, 0].min(), hsvRoi[:, :, 1].min(), hsvRoi[:, :, 2].min()])
