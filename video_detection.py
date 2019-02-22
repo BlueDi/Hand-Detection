0d4fa0520e2fa0466a77ff6e4edb3f404df129f5
Diogo Cruz <diogohmcruz@hotmail.com>
2019-02-22 15:29:23 +0000

    Handle no detection errors
    When the hand isnt being detected some errors would be writtento the console.
    
    Removed those messages as the program is working as expected and those are not errors.

--- video_detection.py
+++ video_detection.py
@@ -142,11 +142,12 @@ def hand_detection(frame, lower_bound_color, upper_bound_color, left):
 
     _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,
                                               cv2.CHAIN_APPROX_SIMPLE)
-
-    cnt = max(contours, key=lambda x: cv2.contourArea(x))
-
-    l = analyse_defects(cnt, roi)
-    analyse_contours(frame, cnt, l + 1)
+    try:
+        cnt = max(contours, key=lambda x: cv2.contourArea(x))
+        l = analyse_defects(cnt, roi)
+        analyse_contours(frame, cnt, l + 1)
+    except ValueError:
+        pass
 
     show_results(binary_mask, mask, frame)
 
@@ -170,27 +171,28 @@ def analyse_defects(cnt, roi):
     defects = cv2.convexityDefects(approx, hull)
 
     l = 0
-    for i in range(defects.shape[0]):
-        s, e, f, d = defects[i, 0]
-        start = tuple(approx[s][0])
-        end = tuple(approx[e][0])
-        far = tuple(approx[f][0])
-        pt = (100, 180)
+    if defects is not None:
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(approx[s][0])
+            end = tuple(approx[e][0])
+            far = tuple(approx[f][0])
+            pt = (100, 180)
 
-        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-        s = (a + b + c) / 2
-        ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
+            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+            s = (a + b + c) / 2
+            ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-        d = (2 * ar) / a
+            d = (2 * ar) / a
 
-        angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
+            angle = math.acos((b**2 + c**2 - a**2) / (2 * b * c)) * 57
 
-        if angle <= 90 and d > 30:
-            l += 1
-            cv2.circle(roi, far, 3, [255, 0, 0], -1)
-        cv2.line(roi, start, end, [0, 255, 0], 2)
+            if angle <= 90 and d > 30:
+                l += 1
+                cv2.circle(roi, far, 3, [255, 0, 0], -1)
+            cv2.line(roi, start, end, [0, 255, 0], 2)
     return l
 
 
